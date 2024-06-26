f1019e2fa914dc2e14f777867c25e724b32bd61c
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-06-26 09:29:11 +0300

    [FIX] Исправление опечаток;

--- assets/classes/characters/professions/wizard_profession.py
+++ assets/classes/characters/professions/wizard_profession.py
@@ -16,7 +16,7 @@ from assets.classes.characters.professions.basic_profession import BasicProfessi
 # КЛАССЫ
 # ##################################################
 
-class WizzardProfession(BasicProfession):
+class WizardProfession(BasicProfession):
     """
     Notes:
         Родительский класс для игровых профессий.
@@ -46,7 +46,87 @@ class WizzardProfession(BasicProfession):
     # ##################################################
     # 1. АТРИБУТЫ КЛАССА
     # ##################################################
-    ...
+    # ВЛАДЕНИЯ
+    _count_start_abilities_posessions: int = 0
+    _available_abilities_posessions: list[str] = [
+        "acrobatics",
+        "analysis",
+        "animal_care",
+        "athletics",
+        "deception",
+        "history",
+        "insight",
+        "intimidation",
+        "magic",
+        "medicine",
+        "nature",
+        "perception",
+        "performance",
+        "persuasion",
+        "religion",
+        "sleight_of_hands",
+        "stealth",
+        "survival"
+    ]
+
+    _available_armor_posessions: list[str] = [
+        "light_armor",
+        "medium_armor",
+        "heavy_armor",
+        "shield"
+    ]
+
+    _available_common_tools_posessions: list[str] = [
+        "disguise_kit",
+        "forgery_kit",
+        "gaming_set",
+        "herbalism_kit",
+        "musical_instruments",
+        "navigator_tools",
+        "poisoners_kit",
+        "thieves_tools"
+    ]
+
+    _available_craft_tools_posessions: list[str] = [
+        "alchemists_supplies",
+        "artisans_tools",
+        "brewer_tools",
+        "calligraphers_supplies",
+        "carpenter_tools",
+        "cartographer_tools",
+        "cobblers_tools",
+        "cooks_utensils",
+        "glass_blower_tools",
+        "jeweler_tools",
+        "letherworker_tools",
+        "masons_tools",
+        "painters_supplies",
+        "potter_tools",
+        "smiths_tools",
+        "tinkers_tools",
+        "weaver_tools",
+        "wood_carver_tools"
+    ]
+
+    _available_transport_posessions: list[str] = [
+        "donkey",
+        "hourse",
+        "wagon",
+        "carriage",
+        "boat",
+        "sailboat",
+        "ship",
+        "warship",
+        "aerostat"
+    ]
+
+    _available_weapon_posessions: list[str] = [
+        "staff",
+        "dagger",
+        "sword",
+        "bow",
+        "crossbow"
+    ]
 
 
     # ##################################################
