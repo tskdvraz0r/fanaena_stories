ade024069777ba59fc9087bd380a146f7b820673
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-06-26 09:33:58 +0300

    [REFACT] Новые атрибуты и свойства;

--- assets/classes/characters/professions/basic_profession.py
+++ assets/classes/characters/professions/basic_profession.py
@@ -48,8 +48,90 @@ class BasicProfession:
     _available_professions: dict[str, str] = {
         "Воин": "Warrior",
         "Следопыт": "Rouge",
-        "Маг": "Wizzard"
+        "Маг": "Wizard"
     }
+
+    # ВЛАДЕНИЯ
+    _count_start_abilities_posessions: int = 0
+    _available_abilities_posessions: list[str] = [
+        "acrobatics",
+        "analysis",
+        "animal_care",
+        "athletics",
+        "deception",
+        "history",
+        "insight",
+        "intimidation",
+        "magic",
+        "medicine",
+        "nature",
+        "perception",
+        "performance",
+        "persuasion",
+        "religion",
+        "sleight_of_hands",
+        "stealth",
+        "survival"
+    ]
+
+    _available_armor_posessions: list[str] = [
+        "light_armor",
+        "medium_armor",
+        "heavy_armor",
+        "shield"
+    ]
+
+    _available_common_tools_posessions: list[str] = [
+        "disguise_kit",
+        "forgery_kit",
+        "gaming_set",
+        "herbalism_kit",
+        "musical_instruments",
+        "navigator_tools",
+        "poisoners_kit",
+        "thieves_tools"
+    ]
+
+    _available_craft_tools_posessions: list[str] = [
+        "alchemists_supplies",
+        "artisans_tools",
+        "brewer_tools",
+        "calligraphers_supplies",
+        "carpenter_tools",
+        "cartographer_tools",
+        "cobblers_tools",
+        "cooks_utensils",
+        "glass_blower_tools",
+        "jeweler_tools",
+        "letherworker_tools",
+        "masons_tools",
+        "painters_supplies",
+        "potter_tools",
+        "smiths_tools",
+        "tinkers_tools",
+        "weaver_tools",
+        "wood_carver_tools"
+    ]
+
+    _available_transport_posessions: list[str] = [
+        "donkey",
+        "hourse",
+        "wagon",
+        "carriage",
+        "boat",
+        "sailboat",
+        "ship",
+        "warship",
+        "aerostat"
+    ]
+
+    _available_weapon_posessions: list[str] = [
+        "staff",
+        "dagger",
+        "sword",
+        "bow",
+        "crossbow"
+    ]
 
 
     # ##################################################
@@ -83,83 +165,64 @@ class BasicProfession:
     # ##################################################
     @property
     def name(self) -> str:
-        """
-        Notes:
-            Геттер, отвечающий за получение наименования Профессии.
-
-        Raises:
-            TypeError: некорректный тип данных.
-
-        Returns:
-            str: наименование Профессии.
-        """
-
         Check.value_type(value=self._name, expected_type=str)
         return self._name
 
     @property
     def health_points_dice(self) -> int:
-        """
-        Notes:
-            Геттер, отвечающий за получение значение кубика, отвечающего за очки телесного здоровья.
-
-        Raises:
-            TypeError: некорректный тип данных.
-
-        Returns:
-            int: значение кубика, отвечающего за очки телесного здоровья.
-        """
-
         Check.value_type(value=self._health_points_dice, expected_type=int)
         return self._health_points_dice
 
     @property
     def mind_points_dice(self) -> int:
-        """
-        Notes:
-            Геттер, отвечающий за получение значение кубика, отвечающего за очки ментального здоровья.
-
-        Raises:
-            TypeError: некорректный тип данных.
-
-        Returns:
-            int: значение кубика, отвечающего за очки ментального здоровья.
-        """
-
         Check.value_type(value=self._mind_points_dice, expected_type=int)
         return self._mind_points_dice
 
     @property
     def mana_points_dice(self) -> int:
-        """
-        Notes:
-            Геттер, отвечающий за получение значение кубика, отвечающего за очки маны.
-
-        Raises:
-            TypeError: некорректный тип данных.
-
-        Returns:
-            int: значение кубика, отвечающего за очки маны.
-        """
-
         Check.value_type(value=self._mana_points_dice, expected_type=int)
         return self._mana_points_dice
 
     @property
     def stamina_points_dice(self) -> int:
-        """
-        Notes:
-            Геттер, отвечающий за получение значение кубика, отвечающего за очки стамины.
-
-        Raises:
-            TypeError: некорректный тип данных.
-
-        Returns:
-            int: значение кубика, отвечающего за очки стамины.
-        """
-
         Check.value_type(value=self._stamina_points_dice, expected_type=int)
         return self._stamina_points_dice
+
+    # ВЛАДЕНИЯ
+    @property
+    def count_start_abilities_posessions(self) -> int:
+        Check.value_type(value=self._count_start_abilities_posessions, expected_type=int)
+        return self._count_start_abilities_posessions
+
+    @property
+    def available_abilities_posessions(self) -> list[str]:
+        Check.value_type(value=self._available_abilities_posessions, expected_type=list)
+        return self._available_abilities_posessions
+
+    @property
+    def available_armor_posessions(self) -> list[str]:
+        Check.value_type(value=self._available_armor_posessions, expected_type=list)
+        return self._available_armor_posessions
+
+    @property
+    def available_common_tools_posessions(self) -> list[str]:
+        Check.value_type(value=self._available_common_tools_posessions, expected_type=list)
+        return self._available_common_tools_posessions
+
+    @property
+    def available_craft_tools_posessions(self) -> list[str]:
+        Check.value_type(value=self._available_craft_tools_posessions, expected_type=list)
+        return self._available_craft_tools_posessions
+
+    @property
+    def available_transport_posessions(self) -> list[str]:
+        Check.value_type(value=self._available_transport_posessions, expected_type=list)
+        return self._available_transport_posessions
+
+    @property
+    def available_weapon_posessions(self) -> list[str]:
+        Check.value_type(value=self._available_weapon_posessions, expected_type=list)
+        return self._available_weapon_posessions
 
 
     # ##################################################
