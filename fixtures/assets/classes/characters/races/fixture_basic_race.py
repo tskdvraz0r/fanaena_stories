89ccb20cca2d3def592bd4e2757d79e4d4a682b3
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-06-29 16:18:00 +0300

    [DEL] Удалена папка фикстур;

--- fixtures/assets/classes/characters/races/fixture_basic_race.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from assets.classes.characters.races.basic_race import BasicRace
-
-@pytest.fixture
-def fixture_basic_race() -> BasicRace:
-    basic_race: BasicRace = BasicRace()
-
-    return basic_race
